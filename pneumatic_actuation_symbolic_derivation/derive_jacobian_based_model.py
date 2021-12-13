c12bed270f24a17cbaa7a01a55be4eae275d3e6a
Maximilian Stölzle <maximilian@stoelzle.ch>
2021-12-13 17:02:59 +0100

    Add model of pneumatic tube

--- pneumatic_actuation_symbolic_derivation/derive_jacobian_based_model.py
+++ pneumatic_actuation_symbolic_derivation/derive_jacobian_based_model.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import sympy
 from sympy.geometry import line
 from sympy.plotting import plot
+from sympy.solvers.diophantine.diophantine import ldescent
 
 """
 This script derives the symbolic model of the pneumatic actuation system for a planar one segment robot.
@@ -15,10 +16,10 @@ class JacobianBasedPneumaticActuationModel:
         self.d_i = d_i # diameter of robot used for kinematic description
         self.r_p = r_p # radius of the center of pressure of the chamber
         self.b_C = b_C # modelled planar depth of the 2D segment
-        self.f_i = 1 # Scalar force acting at the tip of the segment [N] perpendicular to center-line
+        self.f = 1 # Scalar force acting at the tip of the segment [N] perpendicular to center-line
 
         # Symbolic variables
-        self.Delta_i, self.delta_L_i, self.s, self.r = sympy.symbols('Delta_i delta_L_i s r')
+        self.Delta_i, self.delta_L_i, self.s, self.r, self.p = sympy.symbols('Delta_i delta_L_i s r p')
         self.q = sympy.Array([self.Delta_i, self.delta_L_i])
 
         # Scale q with s
@@ -44,7 +45,7 @@ class JacobianBasedPneumaticActuationModel:
         # define normal vector at the tip of the segment
         n_i = self.R @ sympy.Matrix([0, 1])
 
-        tau_i = (self.J.T @ sympy.Matrix(n_i) * self.f_i).subs([(self.s, 1), (self.r, 0)])
+        tau_i = (self.J.T @ sympy.Matrix(n_i) * self.f).subs([(self.s, 1), (self.r, 0)])
 
         line1, line2, line3 = plot(tau_i[0].subs(self.delta_L_i, 1.0*self.L_0_i), 
                                    tau_i[0].subs(self.delta_L_i, 1.1*self.L_0_i), 
@@ -80,7 +81,7 @@ class JacobianBasedPneumaticActuationModel:
         # define normal vector at the tip of the segment
         n_i = self.R @ sympy.Matrix([1, 0])
 
-        tau_i = (self.J.T @ sympy.Matrix(n_i) * self.f_i).subs([(self.s, 1), (self.r, self.r_p)])
+        tau_i = (self.J.T @ sympy.Matrix(n_i) * self.f).subs([(self.s, 1), (self.r, self.r_p)])
 
         line1, line2, line3 = plot(tau_i[0].subs(self.delta_L_i, 1.0*self.L_0_i), 
                                    tau_i[0].subs(self.delta_L_i, 1.1*self.L_0_i), 
@@ -108,7 +109,59 @@ class JacobianBasedPneumaticActuationModel:
         plt.ylabel(r"$\tau_1$ [N]")
         plt.show()
 
+    def pneumatic_tube(self):
+        """
+        Computes and plots a pneumatic tube (e.g. one chamber radially expanding from the CC center-line)
+        :return:
+        """
+
+        # infitesimal torques (not yet integrated along chamber length)
+        dtau_L = (self.J.T @ (self.R @ sympy.Matrix([0, 1])) * self.b_C * self.p).subs([(self.r, self.r_p)])
+        tau_L = sympy.integrate(dtau_L, (self.s, 0, 1))
+        # torque produced along the right side of the tube
+        dtau_R = (self.J.T @ (self.R @ sympy.Matrix([0, -1])) * self.b_C * self.p).subs([(self.r, -self.r_p)])
+        tau_R = sympy.integrate(dtau_R, (self.s, 0, 1))
+        # torque produced along the bottom of the tube (e.g. at the base)
+        J_B_limit = self.J.limit(self.s, 0) # we need to evaluate the limit of J[0] towards s=0, otherwise division by zero
+        dtau_B = (J_B_limit.T @ (self.R @ sympy.Matrix([-1, 0])) * self.b_C * self.p).subs([(self.s, 0)])
+        tau_B = sympy.integrate(dtau_B, (self.r, -self.r_p, self.r_p))
+        # torque produced along the top of the tube (e.g. at the base)
+        dtau_T = (self.J.T @ (self.R @ sympy.Matrix([1, 0])) * self.b_C * self.p).subs([(self.s, 1)])
+        tau_T = sympy.integrate(dtau_T, (self.r, -self.r_p, self.r_p))
+
+        tau = tau_L + tau_R + tau_B + tau_T
+
+        # assume pressure of 1 bar
+        tau = tau.subs(self.p, 10**5)
+
+        line1, line2, line3 = plot(tau[0].subs(self.delta_L_i, 1.0*self.L_0_i), 
+                                   tau[0].subs(self.delta_L_i, 1.1*self.L_0_i), 
+                                   tau[0].subs(self.delta_L_i, 1.2*self.L_0_i), 
+                                   (self.Delta_i, -45/180*pi*self.d_i, 45/180*pi*self.d_i), 
+                                   title="Pneumatic tubing", 
+                                   xlabel=r'$\Delta$ [m]', ylabel=r"$\tau_0$", show=False)
+        x1, y1 = line1.get_points()
+        x2, y2 = line2.get_points()
+        x3, y3 = line3.get_points()
+        plt.plot(x1, y1, x2, y2, x3, y3)
+        plt.title("Pneumatic tubing")
+        plt.xlabel(r'$\Delta_i$ [m]')
+        plt.ylabel(r"$\tau_0$ [N]")
+        plt.legend([r"$\delta L=0 \%$", r"$\delta L=10 \%$", r"$\delta L=20 \%$"])
+        plt.show()
+
+        line1, = plot(tau[1], (self.Delta_i, -45/180*pi*self.d_i, 45/180*pi*self.d_i), 
+                     title="Pneumatic tubing", 
+                     xlabel=r'$\Delta$ [m]', ylabel=r"$\tau_1$ [N]", show=False)
+        x1, y1 = line1.get_points()
+        plt.plot(x1, y1)
+        plt.title("Pneumatic tubing")
+        plt.xlabel(r'$\Delta_i$ [m]')
+        plt.ylabel(r"$\tau_1$ [N]")
+        plt.show()
+
 if __name__ == '__main__':
     model = JacobianBasedPneumaticActuationModel()
-    model.perpendicular_force_at_tip()
-    model.force_at_center_of_pressure_at_tip()
+    # model.perpendicular_force_at_tip()
+    # model.force_at_center_of_pressure_at_tip()
+    model.pneumatic_tube()
