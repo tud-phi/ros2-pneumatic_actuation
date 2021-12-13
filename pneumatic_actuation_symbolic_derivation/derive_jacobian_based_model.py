e5e20c9c7f61e25dca366e6e723fa9bad177b347
Maximilian Stölzle <maximilian@stoelzle.ch>
2021-12-13 16:09:32 +0100

    Add force_at_center_of_pressure_at_tip

--- pneumatic_actuation_symbolic_derivation/derive_jacobian_based_model.py
+++ pneumatic_actuation_symbolic_derivation/derive_jacobian_based_model.py
@@ -9,12 +9,12 @@ This script derives the symbolic model of the pneumatic actuation system for a p
 """
 
 class JacobianBasedPneumaticActuationModel:
-    def __init__(self, L_0_i: float = 1., d_i: float = 1., r_p: float = 1.) -> None:
+    def __init__(self, L_0_i: float = 1., d_i: float = 1., r_p: float = 1., b_C: float = 1.) -> None:
         # Parameters
         self.L_0_i = L_0_i # Unextended length of the arm segment
         self.d_i = d_i # diameter of robot used for kinematic description
         self.r_p = r_p # radius of the center of pressure of the chamber
-        ## Perpendicular force at the tip of the segment
+        self.b_C = b_C # modelled planar depth of the 2D segment
         self.f_i = 1 # Scalar force acting at the tip of the segment [N] perpendicular to center-line
 
         # Symbolic variables
@@ -51,41 +51,64 @@ class JacobianBasedPneumaticActuationModel:
                                    tau_i[0].subs(self.delta_L_i, 1.2*self.L_0_i), 
                                    (self.Delta_i, -45/180*pi*self.d_i, 45/180*pi*self.d_i), 
                                    title="Perpendicular force at the tip of the segment", 
-                                   xlabel=r'$\Delta_i$', ylabel=r"$\tau_0$", show=False)
+                                   xlabel=r'$\Delta$ [m]', ylabel=r"$\tau_0$ [N]", show=False)
         x1, y1 = line1.get_points()
         x2, y2 = line2.get_points()
         x3, y3 = line3.get_points()
         plt.plot(x1, y1, x2, y2, x3, y3)
         plt.title("Perpendicular force at the tip of the segment")
-        plt.xlabel(r'$\Delta_i$')
-        plt.ylabel(r"$\tau_0$")
-        plt.legend([r"$\delta L_i=0 \%$", r"$\delta L_i=10 \%$", r"$\delta L_i=20 \%$"])
+        plt.xlabel(r'$\Delta$ [m]')
+        plt.ylabel(r"$\tau_0$ [N]")
+        plt.legend([r"$\delta L=0 \%$", r"$\delta L=10 \%$", r"$\delta L=20 \%$"])
         plt.show()
 
-        line1 = plot(tau_i[1], (self.Delta_i, -45/180*pi*self.d_i, 45/180*pi*self.d_i), 
+        line1, = plot(tau_i[1], (self.Delta_i, -45/180*pi*self.d_i, 45/180*pi*self.d_i), 
                      title="Perpendicular force at the tip of the segment", 
-                     xlabel=r'$\Delta_i$', ylabel=r"$\tau_1$", show=True)
+                     xlabel=r'$\Delta$ [m]', ylabel=r"$\tau_1$ [N]", show=False)
+        x1, y1 = line1.get_points()
+        plt.plot(x1, y1)
+        plt.title("Perpendicular force at the tip of the segment")
+        plt.xlabel(r'$\Delta$ [m]')
+        plt.ylabel(r"$\tau_1$ [N]")
+        plt.show()
     
-        tau_i = (self.J.T @ sympy.Matrix(n_i) * self.f_i).subs(self.s, 1)
+    def force_at_center_of_pressure_at_tip(self):
+        """
+        Computes and plots normal force at center of pressure of the chamber at the tip of the segment.
+        :return:
+        """
+        # define normal vector at the tip of the segment
+        n_i = self.R @ sympy.Matrix([1, 0])
+
+        tau_i = (self.J.T @ sympy.Matrix(n_i) * self.f_i).subs([(self.s, 1), (self.r, self.r_p)])
 
         line1, line2, line3 = plot(tau_i[0].subs(self.delta_L_i, 1.0*self.L_0_i), 
                                    tau_i[0].subs(self.delta_L_i, 1.1*self.L_0_i), 
                                    tau_i[0].subs(self.delta_L_i, 1.2*self.L_0_i), 
-                                   (self.Delta_i, -45/180*pi*self.d_i, 45/180*pi*self.d_i), title="Perpendicular force at the tip of the segment", 
-                                   xlabel=r'$\Delta_i$', ylabel=r"$\tau_0$", show=False)
+                                   (self.Delta_i, -45/180*pi*self.d_i, 45/180*pi*self.d_i), 
+                                   title="Normal force at center of pressure of the chamber at the tip of the segment", 
+                                   xlabel=r'$\Delta$ [m]', ylabel=r"$\tau_0$", show=False)
         x1, y1 = line1.get_points()
         x2, y2 = line2.get_points()
         x3, y3 = line3.get_points()
         plt.plot(x1, y1, x2, y2, x3, y3)
-        plt.title("Perpendicular force at the tip of the segment")
-        plt.xlabel(r'$\Delta_i$')
-        plt.ylabel(r"$\tau_0$")
-        plt.legend([r"$\delta L_i=0 \%$", r"$\delta L_i=10 \%$", r"$\delta L_i=20 \%$"])
+        plt.title("Normal force at center of pressure of the chamber at the tip of the segment")
+        plt.xlabel(r'$\Delta_i$ [m]')
+        plt.ylabel(r"$\tau_0$ [N]")
+        plt.legend([r"$\delta L=0 \%$", r"$\delta L=10 \%$", r"$\delta L=20 \%$"])
         plt.show()
 
-        line1 = plot(tau_i[1], (self.Delta_i, -45/180*pi*self.d_i, 45/180*pi*self.d_i), title="Perpendicular force at the tip of the segment", 
-                    xlabel=r'$\Delta_i$', ylabel=r"$\tau_1$", show=True)
+        line1, = plot(tau_i[1], (self.Delta_i, -45/180*pi*self.d_i, 45/180*pi*self.d_i), 
+                     title="Normal force at center of pressure of the chamber at the tip of the segment", 
+                     xlabel=r'$\Delta$ [m]', ylabel=r"$\tau_1$ [N]", show=False)
+        x1, y1 = line1.get_points()
+        plt.plot(x1, y1)
+        plt.title("Normal force at center of pressure of the chamber at the tip of the segment")
+        plt.xlabel(r'$\Delta_i$ [m]')
+        plt.ylabel(r"$\tau_1$ [N]")
+        plt.show()
 
 if __name__ == '__main__':
     model = JacobianBasedPneumaticActuationModel()
     model.perpendicular_force_at_tip()
+    model.force_at_center_of_pressure_at_tip()
