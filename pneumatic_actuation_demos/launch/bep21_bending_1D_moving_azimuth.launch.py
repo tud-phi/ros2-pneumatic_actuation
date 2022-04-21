81a2627ac7213b31b3a8a3197f2432ffc2494f95
Maximilian Stoelzle <maximilian@stoelzle.ch>
2022-04-21 11:56:34 +0000

    Add code comment on period of star trajectory

--- pneumatic_actuation_demos/launch/bep21_bending_1D_moving_azimuth.launch.py
+++ pneumatic_actuation_demos/launch/bep21_bending_1D_moving_azimuth.launch.py
@@ -34,7 +34,7 @@ def generate_launch_description():
                     "pressure_peaks": [225*100],
                     "radius_CoP": 0.1,
                     "segment_trajectories": [SegmentTrajectoryType.BENDING_1D_MOVING_AZIMUTH],
-                    "torque_azimuth_frequencies": [0.0025],
+                    "torque_azimuth_frequencies": [0.0025], # period of 400s
                     "trajectory_frequencies": [0.2],
                     "vtem_status_topic": vtem_status_topic,
                     "wait_for_vtem": use_vtem,
